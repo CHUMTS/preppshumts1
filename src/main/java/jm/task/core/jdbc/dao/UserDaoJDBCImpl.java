9bbfce9aa88a1fe86bb487fc4861db9384e2dfba
CHUMTS <humancorrector@gmail.com>
2023-04-18 13:59:34 +0300

    addedTransactions

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    Connection connectionVar;
     public UserDaoJDBCImpl() {
 
     }
@@ -32,29 +33,47 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void saveUser(String name, String lastName, byte age) {
         try (Connection connection = Util.getConnecton()) {
+            connection.setAutoCommit(false);
+            connectionVar = connection;
             PreparedStatement statement = connection.prepareStatement("INSERT INTO userstable (name, lastName, age) VALUES (?, ?, ?)");
             statement.setString(1, name);
             statement.setString(2, lastName);
             statement.setByte(3, age);
             statement.execute();
             System.out.println("User с именем " + name +" добавлен в базу данных");
+            connectionVar.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+                try {
+                    connectionVar.rollback();
+                } catch (SQLException ex) {
+                    System.out.println(">Unsuccessful rollback<");
+                }
         }
     }
 
     public void removeUserById(long id) {
         try (Connection connection = Util.getConnecton()) {
+            connection.setAutoCommit(false);
+            connectionVar = connection;
             PreparedStatement statement = connection.prepareStatement("DELETE FROM userstable WHERE id="+ id);
             statement.execute();
+            connectionVar.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+                try {
+                    connectionVar.rollback();
+                } catch (SQLException ex) {
+                    System.out.println(">Unsuccessful rollback<");
+                }
         }
     }
 
     public List<User> getAllUsers() {
         List<User> result = new ArrayList<>();
         try (Connection connection = Util.getConnecton()) {
+            connection.setAutoCommit(false);
+            connectionVar = connection;
             Statement statement = connection.createStatement();
             ResultSet set = statement.executeQuery("SELECT * FROM userstable");
             while (set.next()) {
@@ -65,18 +84,32 @@ public class UserDaoJDBCImpl implements UserDao {
                 user.setAge(set.getByte("age"));
                 result.add(user);
             }
+            connectionVar.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+                try {
+                    connectionVar.rollback();
+                } catch (SQLException ex) {
+                    System.out.println(">Unsuccessful rollback<");
+                }
         }
         return result;
     }
 
     public void cleanUsersTable() {
         try (Connection connection = Util.getConnecton()) {
+            connection.setAutoCommit(false);
+            connectionVar = connection;
             PreparedStatement statement = connection.prepareStatement("TRUNCATE TABLE userstable");
             statement.execute();
+            connectionVar.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+                try {
+                    connectionVar.rollback();
+                } catch (SQLException ex) {
+                    System.out.println(">Unsuccessful rollback<");
+                }
         }
     }
 }
