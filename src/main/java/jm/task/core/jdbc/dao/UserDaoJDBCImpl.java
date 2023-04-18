2e67e61a3ad8a786a4aea36d34dcb86f80632196
CHUMTS <humancorrector@gmail.com>
2023-04-18 14:01:43 +0300

    addedTransactions

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,14 +1,12 @@
 package jm.task.core.jdbc.dao;
-
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    Connection connectionVar;
+    private Connection connectionVar;
     public UserDaoJDBCImpl() {
 
     }
